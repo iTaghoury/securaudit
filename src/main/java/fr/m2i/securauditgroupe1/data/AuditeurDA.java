20b6ac218f3263b32dca3e6ff154e738bf68a738
iTaghoury <idriss.taghoury@gmail.com>
2023-03-07 11:09:07 +0100

    Created Create query for Auditeur

--- src/main/java/fr/m2i/securauditgroupe1/data/AuditeurDA.java
+++ src/main/java/fr/m2i/securauditgroupe1/data/AuditeurDA.java
@@ -1,17 +1,37 @@
 package fr.m2i.securauditgroupe1.data;
 
+import fr.m2i.securauditgroupe1.model.Auditeur;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class AuditeurDA extends DataAccess implements AutoCloseable {
+
     //region QUERY STRINGS
+
+    private final String INSERT_AUDITEUR_QUERY = "INSERT INTO Auditeur (civilite, nom, prenom) VALUE (?, ?, ?)";
     private final String SELECT_AUDITEUR_QUERY = "SELECT * FROM Auditeur";
+
     //endregion
 
     public AuditeurDA() {
         super();
     }
+
+
+    //region CREATE QUERY
+
+    public void addAuditeurToDB(Auditeur auditeur) throws SQLException {
+        try(PreparedStatement ps = this.getConnection().prepareStatement(INSERT_AUDITEUR_QUERY))  {
+            ps.setString(1, auditeur.getCivilite());
+            ps.setString(2, auditeur.getNom());
+            ps.setString(3, auditeur.getPrenom());
+            ps.execute();
+        }
+    }
+
+    //endregion
 
     //region READ QUERIES
 
