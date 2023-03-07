5df548d7e84f4a71d574588f5ef3c9e742350745
ycronel <caroline-lenorcy@orange.fr>
2023-03-07 11:17:52 +0100

    created the add method for Industrie

--- src/main/java/fr/m2i/securauditgroupe1/api/IndustrieResource.java
+++ src/main/java/fr/m2i/securauditgroupe1/api/IndustrieResource.java
@@ -1,4 +1,34 @@
 package fr.m2i.securauditgroupe1.api;
 
+import fr.m2i.securauditgroupe1.data.IndustrieDA;
+import fr.m2i.securauditgroupe1.model.Industrie;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+import java.sql.SQLException;
+
+@Path("/industrie")
 public class IndustrieResource {
+
+    @POST
+    @Path("/create")
+    public Response createIndustrie(@FormParam("siret") String siret, @FormParam("raisonSociale") String raisonSociale) {
+        Industrie industrie = new Industrie(siret, raisonSociale);
+        try(IndustrieDA da = new IndustrieDA()) {
+            da.addIndustrie(industrie);
+        } catch (SQLException e) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e.getMessage())
+                    .build();
+        }
+        return Response
+                .status(Response.Status.CREATED)
+                .entity(industrie)
+                .build();
+    }
+
+
 }
--- src/main/java/fr/m2i/securauditgroupe1/data/IndustrieDA.java
+++ src/main/java/fr/m2i/securauditgroupe1/data/IndustrieDA.java
@@ -1,4 +1,41 @@
 package fr.m2i.securauditgroupe1.data;
 
-public class IndustrieDA extends DataAccess{
+import fr.m2i.securauditgroupe1.model.Industrie;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class IndustrieDA extends DataAccess implements AutoCloseable {
+
+    //region QUERY STRINGS
+    private final String INSERT_INDUSTRIE_QUERY = "INSERT INTO Industrie (siret, raisonSociale) VALUE (?, ?)";
+    //endregion
+
+    //region CONSTRUCTOR
+    public IndustrieDA() {
+        super();
+    }
+    //endregion
+
+    //region CREATE INDUSTRIE
+    public void addIndustrie(Industrie industrie) throws SQLException {
+        try(PreparedStatement ps = this.getConnection().prepareStatement(INSERT_INDUSTRIE_QUERY)) {
+            ps.setString(1, industrie.getSiret());
+            ps.setString(2, industrie.getRaisonSociale());
+            ps.execute();
+        }
+    }
+    //endregion
+
+    //region READ INDUSTRIE
+    //endregion
+
+    //region UPDATE INDUSTRIE
+    //endregion
+
+    //region DELETE INDUSTRIE
+    //endregion
+
+    //region OTHER METHODS
+    //endregion
 }
