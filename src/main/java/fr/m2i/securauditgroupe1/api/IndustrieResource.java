754b4338b7b74e3aee995bbe02da6c9761ea3146
ycronel <caroline-lenorcy@orange.fr>
2023-03-07 15:51:40 +0100

    created getindustrieList + getIndustrieById methods

--- src/main/java/fr/m2i/securauditgroupe1/api/IndustrieResource.java
+++ src/main/java/fr/m2i/securauditgroupe1/api/IndustrieResource.java
@@ -1,10 +1,10 @@
 package fr.m2i.securauditgroupe1.api;
 
 import fr.m2i.securauditgroupe1.data.IndustrieDA;
+import fr.m2i.securauditgroupe1.exception.IdNotFoundException;
 import fr.m2i.securauditgroupe1.model.Industrie;
-import jakarta.ws.rs.FormParam;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.sql.SQLException;
@@ -16,7 +16,7 @@ public class IndustrieResource {
     @Path("/create")
     public Response createIndustrie(@FormParam("siret") String siret, @FormParam("raisonSociale") String raisonSociale) {
         Industrie industrie = new Industrie(siret, raisonSociale);
-        try(IndustrieDA da = new IndustrieDA()) {
+        try (IndustrieDA da = new IndustrieDA()) {
             da.addIndustrie(industrie);
         } catch (SQLException e) {
             return Response
@@ -29,6 +29,46 @@ public class IndustrieResource {
                 .entity(industrie)
                 .build();
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIndustrieList() {
+        try (IndustrieDA da = new IndustrieDA()) {
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(da.getIndustrieFromDB())
+                    .build();
+        } catch (SQLException e) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity(e.getMessage())
+                    .build();
+        }
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getIndustrieById(@PathParam("id") int idIndustrie) {
+        try (IndustrieDA da = new IndustrieDA()) {
+            return Response
+                    .status(Response.Status.OK)
+                    .entity(da.getIndustrieById(idIndustrie))
+                    .build();
+        } catch (IdNotFoundException e1) {
+            return Response
+                    .status(Response.Status.NOT_FOUND)
+                    .entity(e1.getMessage())
+                    .build();
+        } catch (SQLException e2) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(e2.getMessage())
+                    .build();
+        }
+    }
+
+
 
 
 }
--- src/main/java/fr/m2i/securauditgroupe1/data/IndustrieDA.java
+++ src/main/java/fr/m2i/securauditgroupe1/data/IndustrieDA.java
@@ -1,14 +1,18 @@
 package fr.m2i.securauditgroupe1.data;
 
+import fr.m2i.securauditgroupe1.exception.IdNotFoundException;
 import fr.m2i.securauditgroupe1.model.Industrie;
+import fr.m2i.securauditgroupe1.exception.IdNotFoundException;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class IndustrieDA extends DataAccess implements AutoCloseable {
 
     //region QUERY STRINGS
     private final String INSERT_INDUSTRIE_QUERY = "INSERT INTO Industrie (siret, raisonSociale) VALUE (?, ?)";
+    private final String SELECT_INDUSTRIE_QUERY = "SELECT * FROM Industrie";
+    private final String SELECT_INDUSTRIE_BY_ID = "SELECT * FROM Industrie WHERE idIndustrie = ?";
     //endregion
 
     //region CONSTRUCTOR
@@ -28,6 +32,39 @@ public class IndustrieDA extends DataAccess implements AutoCloseable {
     //endregion
 
     //region READ INDUSTRIE
+    public ArrayList<Industrie> getIndustrieFromDB() {
+        ArrayList<Industrie> industrieList = new ArrayList<>();
+        try(PreparedStatement ps = this.getConnection().prepareStatement(SELECT_INDUSTRIE_QUERY);
+            ResultSet rs = ps.executeQuery())
+        {
+            while(rs.next()) {
+                industrieList.add(new Industrie(rs.getInt("idIndustrie"),
+                                                 rs.getString("siret"),
+                                                 rs.getString("raisonSociale")
+                ));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return industrieList;
+    }
+
+    public Industrie getIndustrieById (int idIndustrie) throws IdNotFoundException {
+        try(PreparedStatement ps = this.getConnection().prepareStatement(SELECT_INDUSTRIE_BY_ID))
+        {
+            ps.setInt(1, idIndustrie);
+            try(ResultSet rs = ps.executeQuery()) {
+                if(rs.next()) {
+                    Industrie industrie = new Industrie(rs.getInt("idIndustrie"), rs.getString("siret"), rs.getString("raisonSociale"));
+                    return industrie;
+                } else {
+                    throw new IdNotFoundException("Id not found");
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     //endregion
 
     //region UPDATE INDUSTRIE
