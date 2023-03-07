8885ae5c5c05dc560a6887bb5de20fb8f282f157
ycronel <caroline-lenorcy@orange.fr>
2023-03-07 10:18:41 +0100

    created industrie model

--- src/main/java/fr/m2i/securauditgroupe1/model/Industrie.java
+++ src/main/java/fr/m2i/securauditgroupe1/model/Industrie.java
@@ -1,4 +1,45 @@
 package fr.m2i.securauditgroupe1.model;
 
 public class Industrie {
+    private int idIndustrie, siret;
+    private String raisonSociale;
+
+    //region CONSTRUCTORS
+    public Industrie(int siret, String raisonSociale) {
+        this.siret = siret;
+        this.raisonSociale = raisonSociale;
+    }
+
+    public Industrie(int idIndustrie, int siret, String raisonSociale) {
+        this.idIndustrie = idIndustrie;
+        this.siret = siret;
+        this.raisonSociale = raisonSociale;
+    }
+    //endregion
+
+    //region GETTERS AND SETTERS
+    public int getIdIndustrie() {
+        return idIndustrie;
+    }
+    public void setIdIndustrie(int idIndustrie) {
+        this.idIndustrie = idIndustrie;
+    }
+
+    public int getSiret() {
+        return siret;
+    }
+
+    public void setSiret(int siret) {
+        this.siret = siret;
+    }
+
+    public String getRaisonSociale() {
+        return raisonSociale;
+    }
+
+    public void setRaisonSociale(String raisonSociale) {
+        this.raisonSociale = raisonSociale;
+    }
+    //endregion
+
 }
